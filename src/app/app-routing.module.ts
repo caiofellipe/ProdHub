2ab3ca752a7fdae0815d5d00296dce3b8da1ba84
Caio <caiofellipe80@gmail.com>
2023-06-16 19:46:45 -0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'plans',
+    loadChildren: () => import('./pages/plans/plans.module').then(m => m.PlansModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
